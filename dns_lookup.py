6a8d00ac87f17b24f7d4a3d12d9c01796b7646f0
Kjmelogy <kmelogy@email.sc.edu>
2026-09-09 13:47:07 -0400

    Implement port scanning functionality
    Added a port scanning feature to check open ports on a given IP address or hostname.

--- dns_lookup.py
+++ dns_lookup.py
@@ -1,13 +1,21 @@
 import socket
+from concurrent.futures import ThreadPoolExecutor
 
-website = input("Enter a website URL (e.g., www.example.com): ")
+target = input(f"Enter an IP address or hostname: ")
+target = socket.gethostbyname(target)
 
-try:
-    result = socket.gethostbyname_ex(website)
-    ip_addresses = result[2]
-    print(f"IP addresses for {website}")
-    for ip in ip_addresses:
-        print(ip)
+def scan_port(port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as scanner:
+        scanner.settimeout(0.05)
+        result = scanner.connect_ex((target, port))
 
-except socket.gaierror:
-    print(f"Error: Unable to resolve the hostname {website}")
+    if result == 0:
+        try:
+            service = socket.getservbyport(port)
+            print(f"Port {port} is OPEN - Service: {service}")
+        except OSError:
+            print(f"Port {port} is OPEN - Service: Unknown")
+
+with ThreadPoolExecutor(max_workers=200) as executor:
+    executor.map(scan_port, range(1, 5001))
+print(f"Port scanning completed for {target}.") 
